8d013e18aef42238e41722fa52acd02853666d9b
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-01-24 21:21:59 +0530

    practiced basics of react and created card

---
+++ src/components/Item.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Item.css";
+
+function Item(props) {
+  const itemName = props.name;
+  return (
+    <div>
+      <p className="nirma">{itemName}</p>
+    </div>
+  );
+}
+
+export default Item;
