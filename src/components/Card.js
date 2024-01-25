b30a7bb04e038f419ae2fc8f147428056e2d3948
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-01-25 22:26:11 +0530

    completed demoShopApp

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,53 +1,10 @@
-import React from "react";
-import Item from "./Item";
-import ItemDate from "./ItemDate";
-import "./Card.css";
+import React from 'react';
 
-function Card() {
-  const response = [
-    {
-      itemName: "Nirma",
-      itemDay: "20",
-      itemMonth: "June",
-      itemyear: "1998",
-    },
-    {
-      itemName: "SurfExcel",
-      itemDay: "22",
-      itemMonth: "july",
-      itemyear: "1999",
-    },
-    {
-      itemName: "555",
-      itemDay: "25",
-      itemMonth: "Aug",
-      itemyear: "2001",
-    },
-  ];
-  return (
-    <div className="card">
-      <Item name={response[0].itemName}>Gaurav</Item>
-      <ItemDate
-        day={response[0].itemDay}
-        month={response[0].itemMonth}
-        year={response[0].itemyear}
-      />
+import './Card.css';
 
-      <Item name={response[1].itemName} />
-      <ItemDate
-        day={response[1].itemDay}
-        month={response[1].itemMonth}
-        year={response[1].itemyear}
-      />
-
-      <Item name={response[2].itemName} />
-      <ItemDate
-        day={response[2].itemDay}
-        month={response[2].itemMonth}
-        year={response[2].itemyear}
-      />
-    </div>
-  );
-}
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return <div className={classes}> {props.children}</div>;
+};
 
 export default Card;
